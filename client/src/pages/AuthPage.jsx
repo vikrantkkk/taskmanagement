2987a93fca7a3944d4510e03861b0fabacf527ee
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2024-09-09 19:46:15 +0530

    main auth page responiseve completed

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -8,29 +8,29 @@ const AuthPage = () => {
   const [isRegister, setIsRegister] = useState(false);
 
   return (
-    <Box className="min-h-screen bg-slate-200 flex items-center justify-center">
-      <Box className="w-1/3 bg-white p-4 rounded-lg flex gap-4 flex-col items-center shadow-lg">
+    <Box className="min-h-screen p-4 sm:p-8 bg-slate-200 flex items-center justify-center">
+      <Box className="w-full max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl bg-white p-4 sm:p-8 rounded-lg flex gap-4 flex-col items-center shadow-lg">
         <Box className="flex justify-center items-center gap-2">
           <SingleStoreIcon />
-          <Box className="font-bold text-2xl">TaskPro</Box>
+          <Box className="font-bold text-xl sm:text-2xl">TaskPro</Box>
         </Box>
-        <Box className="text-[#673AB7] font-bold text-2xl">
+        <Box className="text-[#673AB7] font-bold text-lg sm:text-2xl text-center">
           Hi, Welcome Back
         </Box>
-        <Box className="text-lg text-gray-400">
+        <Box className="text-md sm:text-lg text-gray-400 text-center">
           {isRegister
             ? "Register to create a new account"
             : "Enter your credentials to continue"}
         </Box>
-
-        {/* Conditional rendering of either the Login or Register component */}
-        {isRegister ? <Register /> : <Login />}
-
+        <Box className="w-full">{isRegister ? <Register /> : <Login />}</Box>
         <Button
           onClick={() => setIsRegister(!isRegister)}
           color="secondary"
+          className="w-full"
         >
-          {isRegister ? "Already have an account? Sign In" : "Create an account"}
+          {isRegister
+            ? "Already have an account? Sign In"
+            : "Create an account"}
         </Button>
       </Box>
     </Box>
