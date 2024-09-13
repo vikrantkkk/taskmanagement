a16689eb669c3bdc8c903343672b1f121b3586ed
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2024-09-13 19:54:03 +0530

    vercel json file added

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // Use environment variable for API base URL
       "/api": {
         target: process.env.VITE_API_BASE_URL || "https://backend-taskmanagement-atbi.onrender.com/api/v1",
         changeOrigin: true,
